1a419cb7397bd199b9908edf178058ceffef992c
Arnau Mora <arnyminer.z@gmail.com>
2022-08-26 16:14:42 +0200

    :see_no_evil: Added /build

--- build/core/spiffs/spiffs_cache.cpp.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Arnym\Documents\Arduino\reflow_oven\build\core\spiffs\spiffs_cache.cpp.o: \
- C:\Users\Arnym\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs_cache.cpp \
- C:\Users\Arnym\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs.h \
- C:\Users\Arnym\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs_config.h \
- C:\Users\Arnym\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs_nucleus.h
--- build/core/stdlib_noniso.cpp.d
+++
@@ -1,3 +0,0 @@
-c:\Users\Arnym\Documents\Arduino\reflow_oven\build\core\stdlib_noniso.cpp.o: \
- C:\Users\Arnym\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\stdlib_noniso.cpp \
- C:\Users\Arnym\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\stdlib_noniso.h
